bf821fcd5d62d7c8175d5e55ed7b154cbed52728
Pedro Santos <pedrocarlos.ti@gmail.com>
2024-10-02 17:32:47 +0100

    feat: add navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { AiFillBug } from "react-icons/ai";
+
+const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex justify-between items-center p-4 border-b mb-5 ">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex gap-4">
+        {links.map((link) => (
+          <li
+            key={link.href}
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+          >
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
